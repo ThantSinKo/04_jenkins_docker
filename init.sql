99bff86f89ff9f4da8974a2685d04b6964489b80
ThantSinKo <thantsinko.job@gmail.com>
2025-11-04 18:47:54 +0700

    ...

--- init.sql
+++ init.sql
@@ -7,7 +7,6 @@ CREATE TABLE `attraction` (
   `longitude` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
 INSERT INTO `attraction` (`id`, `name`, `detail`, `coverimage`, `latitude`, `longitude`) VALUES
 (1, 'Phi Phi Islands', 'Phi Phi Islands are a group of islands in Thailand between the large island of Phuket and the Malacca Coastal Strait of Thailand.', 'https://www.melivecode.com/attractions/1.jpg', 7.737619, 98.7068755),
 (2, 'Eiffel Tower', 'Eiffel Tower is one of the most famous structures in the world. Eiffel Tower is named after a leading French architect and engineer. It was built as a symbol of the World Fair in 1889.', 'https://www.melivecode.com/attractions/2.jpg', 48.8583736, 2.2922926),
