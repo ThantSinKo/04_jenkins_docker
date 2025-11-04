c77bb1117bccd8379176bec38ada18421481c711
ThantSinKo <thantsinko.job@gmail.com>
2025-11-04 18:17:31 +0700

    added my name and id

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">Thant Sin Ko 6609169</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
